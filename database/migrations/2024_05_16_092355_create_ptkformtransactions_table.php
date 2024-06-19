54b20eea6d84ca129af26b327164318951d139a1
findryankp <findryankurnia@gmail.com>
2024-06-19 11:08:14 +0700

    fix

--- database/migrations/2024_05_16_092355_create_ptkformtransactions_table.php
+++ database/migrations/2024_05_16_092355_create_ptkformtransactions_table.php
@@ -18,6 +18,22 @@ class CreatePtkformtransactionsTable extends Migration
 			$table->integer('ptkform_id');
 			$table->text('status');
 			$table->integer('user_id');
+            $table->dateTime('applicant')->nullable();
+            $table->dateTime('interview_hc')->nullable();
+            $table->dateTime('psikotest')->nullable();
+            $table->dateTime('interview_user')->nullable();
+            $table->dateTime('interview_direksi')->nullable();
+            $table->dateTime('finalisasi')->nullable();
+            $table->dateTime('mcu')->nullable();
+            $table->dateTime('join')->nullable();
+            $table->float('score_applicant')->nullable();
+            $table->float('score_interview_hc')->nullable();
+            $table->float('score_psikotest')->nullable();
+            $table->float('score_interview_user')->nullable();
+            $table->float('score_interview_direksi')->nullable();
+            $table->float('score_finalisasi')->nullable();
+            $table->float('score_mcu')->nullable();
+            $table->float('score_join')->nullable();
             $table->timestamps();
         });
     }
